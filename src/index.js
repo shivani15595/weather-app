2cd35d5861bcf85b5101cac4120a54d1d1d2ee9e
desai-shivani <54591696+desai-shivani@users.noreply.github.com>
2019-09-20 22:35:05 +0530

    Router related changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/statistics" component={App} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
